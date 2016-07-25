a4ba10df0ba5af74e272facd8c5665b5cea89365
Patrick <patrick@unseenvision.com>
2016-07-25 11:14:14 -0700

    add firstDefender and modify css

--- script.js
+++ script.js
@@ -36,52 +36,74 @@ $(document).ready(function() {
 			},
 		}
 		//Play the game
-		function play() {
-			firstDefender = '';
-            secondDefender = '';
-			thirdDefender = '';
-			charSelected = false;
-			playerWin = false;
-			$('.choice').html('Select Your Character: ')
-			$('#luke, #obi-wan, #vader, #boba').appendTo('.characters');
+	function play() {
+		firstDefender = false;
+		secondDefender = false;
+		thirdDefender = false;
+		charSelected = false;
+		playerWin = false;
+		$('.choice').html('Select Your Character: ')
+		$('#luke, #obi-wan, #vader, #boba').attr('class', 'btn btn-lg char').appendTo('#characters');
+		$('#luke, #obi-wan, #vader, #boba').off('click');
 
-			console.log('Is Character Selected: ' + charSelected);
-		}
-	$('.char').click(function() {
-		$('.choice').html('Your Character: ');
-		if (charSelected) return;
-		charSelected = true;
-		if ($(this).attr('id') == "luke") {
-			$('#obi-wan, #vader, #boba').toggleClass('char enemy').appendTo('.enemies');
-		} else if ($(this).attr('id') == "obi-wan") {
-			$('#luke, #vader, #boba').toggleClass('char enemy').appendTo('.enemies');
-		} else if ($(this).attr('id') == "vader") {
-			$('#luke, #obi-wan, #boba').toggleClass('char enemy').appendTo('.enemies');
-		} else if ($(this).attr('id') == "boba") {
-			$('#luke, #obi-wan, #vader').toggleClass('char enemy').appendTo('.enemies');
-		}
-		console.log('Character Selected: ' + charSelected);
-	});
+		selectChar();
+
+		console.log('Is Character Selected: ' + charSelected);
+		console.log('Is First Defender Selected: ' + firstDefender);
+	}
+	// Select character
+	function selectChar() {
+		$('.char').on('click', function() {
+			$('.choice').html('Your Character: ');
+			if (charSelected) return;
+			charSelected = true;
+			if ($(this).attr('id') == "luke") {
+				$('#obi-wan, #vader, #boba').toggleClass('char enemy').appendTo('#enemies');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "obi-wan") {
+				$('#luke, #vader, #boba').toggleClass('char enemy').appendTo('#enemies');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "vader") {
+				$('#luke, #obi-wan, #boba').toggleClass('char enemy').appendTo('#enemies');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "boba") {
+				$('#luke, #obi-wan, #vader').toggleClass('char enemy').appendTo('#enemies');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			}
+
+			selectFirst();
+			console.log('Character Selected: ' + charSelected);
+		});
+	}
 
 	// Select a defender
-	$('.enemy').click(function() {
-		// if (game.play.defSelected === true) {
-		// 	return;
-		// }
-		if ($(this).attr('id') == "luke") {
-			$('#luke').attr('class', 'btn btn-lg defender').appendTo('.defender');
-		} else if ($(this).attr('id') == "obi-wan") {
-			$('#luke, #vader, #boba').attr('class', 'btn btn-lg enemy').appendTo('.enemies');
-		} else if ($(this).attr('id') == "vader") {
-			$('#luke, #obi-wan, #boba').attr('class', 'btn btn-lg enemy').appendTo('.enemies');
-		} else if ($(this).attr('id') == "boba") {
-			$('#luke, #obi-wan, #vader').attr('class', 'btn btn-lg enemy').appendTo('.enemies');
-		}
-	});
-	// Fight
+	function selectFirst() {
+		$('.enemy').on('click', function() {
+			if (firstDefender) return;
+			firstDefender = true;
+			if ($(this).attr('id') == "luke") {
+				$('#luke').toggleClass('enemy defender').appendTo('#defender');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "obi-wan") {
+				$('#obi-wan').toggleClass('enemy defender').appendTo('#defender');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "vader") {
+				$('#vader').toggleClass('enemy defender').appendTo('#defender');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			} else if ($(this).attr('id') == "boba") {
+				$('#boba').toggleClass('enemy defender').appendTo('#defender');
+				$('#luke, #obi-wan, #vader, #boba').off('click');
+			}
+			console.log('First Defender Selected: ' + firstDefender);
+		});
+	}
+	// BATTLE!
+	function fight() {
+
+	}
 
 	// Start New Game
-	$('#reset').click(function() {
+	$('#reset').on('click', function() {
 		play();
 	});
 	play();
